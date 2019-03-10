0d12a86f1eca6743ca1a47eb8813e41c462f11ea
H-OO <hjyuanyy@gmail.com>
2019-03-11 00:43:10 +0800

    update

--- base-app/t.js
+++ base-app/t.js
@@ -1,75 +1,73 @@
-// async function a() {
-//   console.log('a start')
-//   await b() // resolve(b())
-//   console.log('a end')
-// }
-// async function b() {
-//   console.log('b start & end')
-//   // console.log('b start')
-//   // await ''
-//   // console.log('b end')
-// }
-
-// a()
-
-// new Promise((resolve) => {
-//   console.log('promise start')
-//   resolve()
-// }).then(() => {
-//   console.log('promise end')
-// })
-
-// --- node
-// a start
-// b start & end
-// promise start
-// a end
-// promise end
-
-// --- brower
-// a start
-// b start & end
-// promise start
-// promise end
-// a end
-
-function a() {
-  return new Promise(resolve => {
-    console.log('a start') // 1
-    resolve(b())
-  })
-    .then(() => {
-      console.log('a end') // ???
-    })
+async function a() {
+  console.log('a start')
+  await b() // resolve(b())
+  console.log('a end')
+}
+async function b() {
+  console.log('b start & end')
+  // console.log('b start')
+  // await ''
+  // console.log('b end')
 }
 
-function b() {
-  return new Promise(resolve => {
-    console.log('b start') // 2
-    resolve()
-  })
-}
-
+console.log('script start')
 a()
 
 new Promise(resolve => {
-  console.log('promise start') // 3
+  console.log('promise start')
   resolve()
+}).then(() => {
+  console.log('promise end')
 })
-  .then(() => {
-    console.log('promise end') // ???
-  })
 
-// node
+console.log('script end')
+
 
 // --- node
 // a start
-// b start
+// b start & end
+// promise start
 // a end
-// promise
+// promise end
 
 // --- brower
 // a start
-// b start
-// promise
+// b start & end
+// promise start
+// promise end
 // a end
+
+// ---------
+// function a() {
+//   return new Promise(resolve => {
+//     console.log('a start')
+//     resolve(b())
+//   })
+//     .then(() => {
+//       console.log('a end')
+//     })
+// }
+
+// function b() {
+//   return new Promise(resolve => {
+//     console.log('b start')
+//     resolve()
+//   })
+//     .then(() => {
+//       console.log('b end')
+//     })
+// }
+
+// console.log('script start')
+// a()
+
+// new Promise(resolve => {
+//   console.log('promise start')
+//   resolve()
+// })
+//   .then(() => {
+//     console.log('promise end')
+//   })
+
+// console.log('script end')
+
