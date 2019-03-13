e9305e5502b2a5d6b9c0e0889542f0408e385a20
H-OO <38312623+H-OO@users.noreply.github.com>
2019-03-13 20:19:23 +0800

    update

--- base-app/src/components/App/App.jsx
+++ base-app/src/components/App/App.jsx
@@ -4,14 +4,46 @@ import './App.css'
 import T from '../T/T'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      num: 0
+    }
+    this.handler = this.handler.bind(this)
+  }
+  componentWillMount() {
+    console.log('componentWillMount')
+  }
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+  shouldComponentUpdate(newProps, newState) {
+    console.log('shouldComponentUpdate')
+    return true
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate')
+  }
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+  }
+
+  handler() {
+    console.log('handler')
+    this.setState({
+      num: this.state.num + 1
+    })
+  }
+
   render() {
-    console.log(<div></div>)
-    console.log(<T />)
+    console.log('render')
     return (
       <div className="App">
         <T style={{backgroundColor:'red',color:'white',fontSize:40}}>
           <div>abc</div>
         </T>
+        <button onClick={this.handler}>change state</button>
       </div>
     )
   }
--- base-app/src/components/T/T.jsx
+++ base-app/src/components/T/T.jsx
@@ -1,22 +1,53 @@
 import React, { Component } from 'react'
 
-// class T extends Component {
-//   // constructor(props) {
-//   //   super(props)
-//   // }
-//   render() {
-//     console.log(this)
-//     return (
-//       <div>is T Component Content</div>
-//     )
-//   }
-// }
+class T extends Component {
+  constructor() {
+    super()
+    this.state = {
+      t: '123'
+    }
+  }
+  componentWillMount() {
+    console.log('componentWillMount 1')
+    function A() {}
+    console.dir(A)
+  }
+  componentDidMount() {
+    console.log('componentDidMount 2')
+  }
+  componentWillReceiveProps() {
+    console.log('componentWillReceiveProps 3')
+  }
+  shouldComponentUpdate(newProps, newState) {
+    console.log('shouldComponentUpdate 4')
+    return true
+  }
 
-const T = props => {
-  console.log(props)
-  return (
-    <div style={props.style}>{props.children}</div>
-  )
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate 5')
+  }
+  componentDidUpdate() {
+    console.log('componentDidUpdate 6')
+  }
+  
+  componentWillUnmount() {
+    console.log('componentWillUnmount 7')
+  }
+
+  render() {
+    console.log(this)
+    return (
+      <div>is T Component Content</div>
+    )
+  }
 }
+
+// const T = props => {
+//   console.log(props)
+//   return (
+//     <div style={props.style}>{props.children}</div>
+//   )
+// }
 
 export default T
