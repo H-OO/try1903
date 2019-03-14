3adc8cbad1d29359abec191821d1b95cc877465a
H-OO <38312623+H-OO@users.noreply.github.com>
2019-03-14 17:39:40 +0800

    update

--- base-app/src/components/App/App.jsx
+++ base-app/src/components/App/App.jsx
@@ -1,49 +1,19 @@
 import React, { Component } from 'react'
 import './App.css'
 
-import T from '../T/T'
+import { connect } from 'react-redux'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      num: 0
-    }
-    this.handler = this.handler.bind(this)
-  }
-  componentWillMount() {
-    console.log('componentWillMount')
-  }
-  componentDidMount() {
-    console.log('componentDidMount')
-  }
-  shouldComponentUpdate(newProps, newState) {
-    console.log('shouldComponentUpdate')
-    return true
-  }
-
-  componentWillUpdate() {
-    console.log('componentWillUpdate')
-  }
-  componentDidUpdate() {
-    console.log('componentDidUpdate')
-  }
-
   handler() {
     console.log('handler')
     this.setState({
       num: this.state.num + 1
     })
   }
-
   render() {
     console.log('render')
     return (
       <div className="App">
-        <T style={{backgroundColor:'red',color:'white',fontSize:40}}>
-          <div>abc</div>
-        </T>
-        <button onClick={this.handler}>change state</button>
       </div>
     )
   }
